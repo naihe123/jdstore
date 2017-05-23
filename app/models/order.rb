11e3cc90e0c34acd68e16ac31fbd673cc582c641
annqin <253340618@qq.com>
2017-05-24 07:13:54 +0800

    users can see their orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :user
+  before_create :generate_token
+
+  def generate_token
+    self.token = SecureRandom.uuid
+  end
 
   validates :billing_name, presence: true
   validates :billing_address, presence: true
   validates :shipping_name, presence: true
   validates :shipping_address, presence: true
-  def generate_token
-    self.token = SecureRandom.uuid
-  end
+
 
   has_many :product_lists
 end
